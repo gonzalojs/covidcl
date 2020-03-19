7476c56a7d98db219dc90b4654be0c9d054d5807
gonzalojs <eogutier@gmail.com>
2020-03-18 23:15:33 -0300

    components

--- components/Top.vue
+++ components/Top.vue
@@ -27,25 +27,142 @@
                       </div>
                     </div>
                   </div>
-                  <nav class="level is-mobile">
-                    <div class="level-left">
-                      <a class="level-item" aria-label="reply">
-                        <span class="icon is-small">
-                          <i class="fas fa-reply" aria-hidden="true"></i>
-                        </span>
-                      </a>
-                      <a class="level-item" aria-label="retweet">
-                        <span class="icon is-small">
-                          <i class="fas fa-retweet" aria-hidden="true"></i>
-                        </span>
-                      </a>
-                      <a class="level-item" aria-label="like">
-                        <span class="icon is-small">
-                          <i class="fas fa-heart" aria-hidden="true"></i>
-                        </span>
-                      </a>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+        <div class="columns is-mobile is-centered">
+      <div class="column">
+        <div class="columns is-mobile is-centered">
+          <div class="column is-half">
+            <div class="box">
+              <article class="media">
+                <div class="media-left">
+                  <figure class="image is-128x128">
+                    <img src="~/assets/covid.svg" alt="Image" />
+                  </figure>
+                </div>
+                <div class="media-content">
+                  <div class="content">
+                    <div class="columns is-mobile is-centered">
+                      <div class="column">
+                        <progress class="progress is-danger" value="100" max="100">100%</progress>
+                        <div class="columns is-mobile">
+                          <div class="column">
+                            <p class=" title has-text-centered">Infectados</p>
+                          </div>
+                          <div class="column">
+                            <p class="title has-text-centered">10000</p>
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                  </nav>
+                  </div>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+        <div class="columns is-mobile is-centered">
+      <div class="column">
+        <div class="columns is-mobile is-centered">
+          <div class="column is-half">
+            <div class="box">
+              <article class="media">
+                <div class="media-left">
+                  <figure class="image is-128x128">
+                    <img src="~/assets/covid.svg" alt="Image" />
+                  </figure>
+                </div>
+                <div class="media-content">
+                  <div class="content">
+                    <div class="columns is-mobile is-centered">
+                      <div class="column">
+                        <progress class="progress is-danger" value="100" max="100">100%</progress>
+                        <div class="columns is-mobile">
+                          <div class="column">
+                            <p class=" title has-text-centered">Infectados</p>
+                          </div>
+                          <div class="column">
+                            <p class="title has-text-centered">10000</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+        <div class="columns is-mobile is-centered">
+      <div class="column">
+        <div class="columns is-mobile is-centered">
+          <div class="column is-half">
+            <div class="box">
+              <article class="media">
+                <div class="media-left">
+                  <figure class="image is-128x128">
+                    <img src="~/assets/covid.svg" alt="Image" />
+                  </figure>
+                </div>
+                <div class="media-content">
+                  <div class="content">
+                    <div class="columns is-mobile is-centered">
+                      <div class="column">
+                        <progress class="progress is-danger" value="100" max="100">100%</progress>
+                        <div class="columns is-mobile">
+                          <div class="column">
+                            <p class=" title has-text-centered">Infectados</p>
+                          </div>
+                          <div class="column">
+                            <p class="title has-text-centered">10000</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+        <div class="columns is-mobile is-centered">
+      <div class="column">
+        <div class="columns is-mobile is-centered">
+          <div class="column is-half">
+            <div class="box">
+              <article class="media">
+                <div class="media-left">
+                  <figure class="image is-128x128">
+                    <img src="~/assets/covid.svg" alt="Image" />
+                  </figure>
+                </div>
+                <div class="media-content">
+                  <div class="content">
+                    <div class="columns is-mobile is-centered">
+                      <div class="column">
+                        <progress class="progress is-danger" value="100" max="100">100%</progress>
+                        <div class="columns is-mobile">
+                          <div class="column">
+                            <p class=" title has-text-centered">Infectados</p>
+                          </div>
+                          <div class="column">
+                            <p class="title has-text-centered">10000</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </article>
             </div>
