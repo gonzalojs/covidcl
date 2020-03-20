80a02bec25d76ddca8a28d6c6cd3cc172756d6d2
gonzalojs <eogutier@gmail.com>
2020-03-19 21:12:47 -0300

    components

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <Top />
-    <Boxes />
     <Tiles />
     <Footer />
   </div>
@@ -10,14 +9,12 @@
 <script>
 import Footer from "~/components/Footer.vue";
 import Tiles from "~/components/Tiles.vue";
-import Boxes from "~/components/Boxes.vue";
 import Top from "~/components/Top.vue";
 
 export default {
   components: {
     Footer,
     Tiles,
-    Boxes,
     Top
   }
 };
